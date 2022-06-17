83bdafdc42d93143f54f84a235f16fa7f97a6acb
DavidChavess <davi.ch.fe@gmail.com>
2022-06-17 17:08:52 -0300

    refactor: remove blank spaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -14,19 +14,15 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
-
     const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
-
     if (!isEmailValid) {
       return badRequest(new InvalidParamError('email'))
     }
-
     return {
       statusCode: 200,
       body: {}
