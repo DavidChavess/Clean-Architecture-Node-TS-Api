191af346ccfb6deef97614106769919b96caa8bc
DavidChavess <davi.ch.fe@gmail.com>
2022-06-18 11:55:39 -0300

    refactor: remove code dupliation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -17,10 +17,11 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.passwordConfirmation !== httpRequest.body.password) {
+      const { password, passwordConfirmation, email } = httpRequest.body
+      if (passwordConfirmation !== password) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email)
+      const isEmailValid = this.emailValidator.isValid(email)
       if (!isEmailValid) {
         return badRequest(new InvalidParamError('email'))
       }
