7dbf81b735c80d972d1464e3d06c6ae4237d7f27
DavidChavess <davi.ch.fe@gmail.com>
2022-11-08 21:05:36 -0300

    refactor: add mapper method in MongoHelper

--- src/infra/db/mongodb/account-repository/account-mapper.ts
+++
@@ -1,8 +0,0 @@
-import { InsertOneResult } from 'mongodb'
-import { AccountModel } from '../../../../domain/models/account-model'
-import { AddAccountModel } from '../../../../domain/usecases/add-account'
-
-export const map = (account: AddAccountModel, resultBd: InsertOneResult): AccountModel => {
-  const id = resultBd.insertedId.toString()
-  return Object.assign({}, account, { id })
-}
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,12 +2,11 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AccountModel } from '../../../../domain/models/account-model'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { map } from './account-mapper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
-    const result = await accountCollection.insertOne(account)
-    return map(account, result)
+    await accountCollection.insertOne(account)
+    return MongoHelper.map(account)
   }
 }
