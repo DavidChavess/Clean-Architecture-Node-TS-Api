2d56dba85184a3e7cec319318cb4da243f5d0ec7
DavidChavess <davi.ch.fe@gmail.com>
2022-11-21 20:25:04 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -9,16 +9,22 @@ class ValidationStub implements Validation {
 }
 
 describe('Validation Composite', () => {
-  let _validationStub: ValidationStub
+  let _validationStubs: ValidationStub[]
   let _sut: ValidationComposite
 
   beforeEach(() => {
-    _validationStub = new ValidationStub()
-    _sut = new ValidationComposite([_validationStub])
+    _validationStubs = [new ValidationStub(), new ValidationStub()]
+    _sut = new ValidationComposite(_validationStubs)
   })
 
   test('Should return an error if any validation fails', () => {
-    jest.spyOn(_validationStub, 'validate').mockReturnValueOnce(new MissingParamError('name'))
+    jest.spyOn(_validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamError('name'))
     expect(_sut.validate({ email: 'any_email' })).toEqual(new MissingParamError('name'))
+  })
+
+  test('Should return the first error if more than one validation fails', () => {
+    jest.spyOn(_validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(_validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamError('name'))
+    expect(_sut.validate({ email: 'any_email' })).toEqual(new Error())
   })
 })
