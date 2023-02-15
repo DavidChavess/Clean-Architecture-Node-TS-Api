692d02704bb4c35f62f1c90ec0cf3e21adacaa4e
DavidChavess <davi.ch.fe@gmail.com>
2023-02-15 15:22:18 -0300

    feat: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,38 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+const makeAddSurveyModel = (): any => ({
+  question: 'any_question',
+  answers: [{
+    image: 'any_image',
+    answer: 'any_answer'
+  }]
+})
+describe('Survey Mongo Repository', () => {
+  let accountCollection: Collection
+
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('surveys')
+    await accountCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should add a new survey on add success', async () => {
+    const sut = makeSut()
+    await sut.add(makeAddSurveyModel())
+    const response = await accountCollection.findOne({ question: 'any_question' })
+    expect(response).toBeTruthy()
+  })
+})
---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,10 @@
+import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (survey: AddSurveyModel): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('surveys')
+    await accountCollection.insertOne(survey)
+  }
+}
