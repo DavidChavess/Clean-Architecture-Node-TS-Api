910730ccdb5a0adfb06f706ed5d4df903aa315f7
DavidChavess <davi.ch.fe@gmail.com>
2023-04-23 10:32:52 -0300

    refactor: move sut creation to a beforeEach method

--- src/data/usecases/survey-result/load/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load/db-load-survey-result.spec.ts
@@ -1,12 +1,19 @@
 import { mockLoadSurveyResultRepository } from '@/data/test'
 import { DbLoadSurveyResult } from './db-load-survey-result'
+import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
 
 describe('DbLoadSurveyResult Usecase', () => {
+  let _loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+  let _sut: DbLoadSurveyResult
+
+  beforeEach(() => {
+    _loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+    _sut = new DbLoadSurveyResult(_loadSurveyResultRepositoryStub)
+  })
+
   test('Should call loadSurveyRepository with correct survey id', async () => {
-    const loadSurveyResultRepository = mockLoadSurveyResultRepository()
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepository)
-    const loadSpy = jest.spyOn(loadSurveyResultRepository, 'loadBySurveyId')
-    await sut.load('any_survey_id')
+    const loadSpy = jest.spyOn(_loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await _sut.load('any_survey_id')
     expect(loadSpy).toHaveBeenCalledWith('any_survey_id')
   })
 })
