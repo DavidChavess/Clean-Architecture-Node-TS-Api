b810f175732608ae9593ce1fe334179bbe190685
DavidChavess <davi.ch.fe@gmail.com>
2023-04-27 20:44:20 -0300

    feat: ensure LoadSurveyResultController returns 403 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/load/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load/load-survey-result-controller.spec.ts
@@ -1,16 +1,27 @@
 import { mockLoadSurveyById } from '@/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
+import { LoadSurveyById } from '@/data/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols'
+import { forbidden } from '@/presentation/helpers/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
 
 describe('LoadSurveyResult Controller', () => {
+  let _loadSurveyById: LoadSurveyById
+  let _sut: LoadSurveyResultController
+
+  beforeEach(() => {
+    _loadSurveyById = mockLoadSurveyById()
+    _sut = new LoadSurveyResultController(_loadSurveyById)
+  })
+
   test('Should call LoadSurveyById with correct survey id', async () => {
-    const loadSurveyById = mockLoadSurveyById()
-    const sut = new LoadSurveyResultController(loadSurveyById)
-    const loadSpy = jest.spyOn(loadSurveyById, 'loadById')
-    await sut.handle({
-      params: {
-        surveyId: 'any_survey_id'
-      }
-    })
+    const loadSpy = jest.spyOn(_loadSurveyById, 'loadById')
+    await _sut.handle({ params: { surveyId: 'any_survey_id' } })
     expect(loadSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  test('Should return 403 if LoadSurveyById returns null', async () => {
+    jest.spyOn(_loadSurveyById, 'loadById').mockResolvedValueOnce(null)
+    const httpResponse = await _sut.handle({ params: { surveyId: 'any_survey_id' } })
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')))
   })
 })
