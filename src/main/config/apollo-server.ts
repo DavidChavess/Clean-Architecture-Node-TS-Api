ede9ac5ad7bc98712ae09ddaff58f73010abca9e
DavidChavess <davi.ch.fe@gmail.com>
2023-06-09 17:16:00 -0300

    feat: setup apollo server

---
+++ src/main/config/apollo-server.ts
@@ -0,0 +1,14 @@
+import typeDefs from '@/main/graphql/typedefs'
+import resolvers from '@/main/graphql/resolvers'
+
+import { ApolloServer } from 'apollo-server-express'
+import { Express } from 'express'
+
+export default async (app: Express): Promise<void> => {
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs
+  })
+  await server.start()
+  server.applyMiddleware({ app })
+}
---
+++ src/main/graphql/resolvers/login.ts
@@ -0,0 +1,8 @@
+export default {
+  Query: {
+    login: () => ({
+      accessToken: 'any token',
+      name: 'any name'
+    })
+  }
+}
