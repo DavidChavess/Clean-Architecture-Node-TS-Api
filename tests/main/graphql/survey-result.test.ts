4e18469888b179a418f82b3b70b06a4282186ef0
DavidChavess <davi.ch.fe@gmail.com>
2023-06-15 09:33:11 -0300

    test: ensure SurveyResult Query returns 200 on load surveyResult with valid accessToken

--- tests/main/graphql/survey-result.test.ts
+++ tests/main/graphql/survey-result.test.ts
@@ -2,6 +2,7 @@ import app from '@/main/config/app'
 import request from 'supertest'
 import { MongoHelper } from '@/infra/db'
 import { Collection } from 'mongodb'
+import { mockSurveyParams, mockAccessToken } from '@/tests/main/graphql/mocks'
 
 describe('SurveyResult GraphQL', () => {
   let surveyCollection: Collection
@@ -45,6 +46,35 @@ describe('SurveyResult GraphQL', () => {
         .post('/graphql')
         .send({ query: query() })
         .expect(403)
+    })
+
+    test('Should return 200 on load surveyResult with valid accessToken', async () => {
+      const date = new Date()
+      const survey = { ...mockSurveyParams(), date }
+      await surveyCollection.insertOne(survey)
+
+      const { status, body: { data: { surveyResult } } } = await request(app)
+        .post('/graphql')
+        .set('x-access-token', await mockAccessToken(accountCollection))
+        .send({ query: query(survey._id) })
+
+      expect(surveyResult.question).toBe(survey.question)
+      expect(surveyResult.date).toBe(date.toISOString())
+      expect(surveyResult.answers).toEqual([
+        {
+          answer: 'any_answer',
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: false
+        },
+        {
+          answer: 'any_answer_2',
+          count: 0,
+          percent: 0,
+          isCurrentAccountAnswer: false
+        }
+      ])
+      expect(status).toBe(200)
     })
   })
 })
